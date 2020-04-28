a013405741f3b8647f9ed9ef58a81d9db53f2d45
haifengMC <1745502795@qq.com>
2020-04-28 10:08:28 +0800

    负数取余

---
+++ 03_运算符/02_负数取余/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int i1 = 2;
+	int i2 = 1;
+	int i3 = 0;
+	int i4 = -1;
+	int i5 = -2;
+
+	cout << i1 % 2 << endl;
+	cout << i2 % 2 << endl;
+	cout << i3 % 2 << endl;
+	cout << i4 % 2 << endl;
+	cout << i5 % 2 << endl;
+
+	return 0;
+}
