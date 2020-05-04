136c06c92847cc8d71ce057944e7a2820650fcdb
haifengMC <1745502795@qq.com>
2020-05-04 17:04:33 +0800

    文件读写追加测试

--- 07_文件/01_文件读写/main.cpp
+++ 07_文件/01_文件读写/main.cpp
@@ -6,23 +6,30 @@ using namespace std;
 int main()
 {
 	FILE* infile, *outfile;
-	fopen_s(&outfile, "file", "wb");
+	fopen_s(&outfile, "file.txt", "wb");
 	char buf1[4] = "123";
 	cout << fwrite(buf1, 1, sizeof(buf1), outfile) << endl;
 	fclose(outfile);
 
 
-	fopen_s(&infile, "file", "rb");
+	fopen_s(&infile, "file.txt", "rb");
 
 	if (!infile) return 0;
 
 	fseek(infile, 0, SEEK_END);
 	int len = ftell(infile);
-	cout << len << endl;
-	fseek(infile, 0, SEEK_SET);
+	cout << len << endl;	fseek(infile, 0, SEEK_SET);
 	char buf2[4] = "";
-	cout << fread(buf2, 1, len, infile) << endl;
+	fread(buf2, 1, len, infile);
 	fclose(infile);
+
+	FILE* outfile2;
+	fopen_s(&outfile2, "file.txt", "ab");
+	fseek(infile, 3, SEEK_SET);
+	char buf3[4] = "456";
+	cout << fwrite(buf3, 1, sizeof(buf3), outfile2) << endl;
+	fclose(outfile2);
+
 
 	return 0;
 }
