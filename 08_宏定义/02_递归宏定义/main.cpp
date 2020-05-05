597a20f6a50f75e9d43d3c7f3f15841cdb192546
haifengMC <1745502795@qq.com>
2020-05-05 15:44:06 +0800

    测试宏递归

---
+++ 08_宏定义/02_递归宏定义/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+
+#define TO_STRING(X) TO_STRING1(X)
+#define TO_STRING1(X) #X
+
+#define COMB(X, Y) COMB1(X, Y)
+#define COMB1(X, Y) X##Y
+
+#define COMMA(X) ,X
+
+
+#define DEC_1 0
+#define DEC_2 1
+#define DEC_3 2
+#define DEC(n) COMB(DEC_, n)
+
+#define REPEAT_1(n, f, e) f(n, e)
+#define REPEAT_2(n, f, e) COMB(COMB(REPEAT_, DEC(n))(DEC(n), f, e), COMMA(f(n, e)))
+#define REPEAT_3(n, f, e) COMB(COMB(REPEAT_, DEC(n))(DEC(n), f, e), COMMA(f(n, e)))
+
+#define TEST1(n, x) x 
+#define TEST2(n, x) COMB(x, n)
+
+#define ARGS(type, n) 
+
+int main()
+{
+	cout << TO_STRING(COMB(1, 2)) << endl;
+	cout << TO_STRING(DEC(3)) << endl;
+	cout << TO_STRING(REPEAT_2(2, TEST1, X)) << endl;
+	cout << TO_STRING(REPEAT_3(3, TEST2, X)) << endl;
+
+
+	return 0;
+}
