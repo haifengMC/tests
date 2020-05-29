9ed9f3f5ccf259ba28d8fae4b207824dfa29c117
haifengMC <1745502795@qq.com>
2020-05-29 11:44:34 +0800

    EXIST适应新版REPEAT

--- 08_宏定义/14_多参数REPEAT/main.cpp
+++ 08_宏定义/14_多参数REPEAT/main.cpp
@@ -231,8 +231,8 @@ using namespace std;
 #define GET_SAFE_RET(var, ret, ...) (EXPAND(COMB(GET_SAFE_, EXPAND(NUM(__VA_ARGS__)))(var, ret, EXPAND(NUM(__VA_ARGS__)), __VA_ARGS__)))
 #define GET_SAFE(var, ...) GET_SAFE_RET(var, 0, __VA_ARGS__)
 
-#define EXIST_F(n, X) EXCP_M(X)
-#define EXIST(ret, ...) if(EXPAND(REPEAT_VA_SEP(EXIST_F, ORM, __VA_ARGS__))) return ret;
+#define EXIST_F(n, X) (EXCP_M((X)))
+#define EXIST(ret, ...) if(EXPAND(REPEAT_SEP(EXIST_F, OR_M, __VA_ARGS__))) return ret;
 #define EXIST_V(...) EXIST(void, __VA_ARGS__)
 #define EXIST_B(...) EXIST(false, __VA_ARGS__)
 #define EXIST_I(...) EXIST(0, __VA_ARGS__)
@@ -287,6 +287,10 @@ int main()
 	cout << TO_STRING(REPEAT_N_F_ZERO(4, F2, 2, a, b, c, d)) << endl;
 	cout << TO_STRING(REVERSE_REPEAT_N_F(4, F2, 2, a, b, c, d)) << endl;
 	cout << TO_STRING(REVERSE_REPEAT_N_F_ZERO(4, F2, 2, a, b, c, d)) << endl;
+	cout << "........." << endl;
+
+	cout << TO_STRING(EXIST(*this, a, b, c, d)) << endl;
+	cout << TO_STRING(GET_SAFE(a, 1, 2, 3)) << endl;
 
 	return 0;
 }
