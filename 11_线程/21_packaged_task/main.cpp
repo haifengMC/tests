b38a3b3b08419e960c3f09c433d7a023605ff8e4
haifengMC <1745502795@qq.com>
2020-06-20 16:53:12 +0800

    测试packaged_task

---
+++ 11_线程/21_packaged_task/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+#include <thread>
+#include <future>
+#include <functional>
+
+// 避免对 std::pow 重载集消歧义的独有函数
+int f(int x, int y) 
+{
+	std::this_thread::sleep_for(std::chrono::seconds(1)); 
+	return std::pow(x, y);
+}
+
+void task_lambda()
+{
+	std::packaged_task<int(int, int)> task([](int a, int b) {
+		return std::pow(a, b);
+		});
+	std::future<int> result = task.get_future();
+
+	task(2, 9);
+
+	std::cout << "task_lambda:\t" << result.get() << '\n';
+}
+
+void task_bind()
+{
+	std::packaged_task<int()> task(std::bind(f, 2, 11));
+	std::future<int> result = task.get_future();
+
+	task();
+
+	std::cout << "task_bind:\t" << result.get() << '\n';
+}
+
+void task_thread()
+{
+	std::packaged_task<int(int, int)> task(f);
+	std::future<int> result = task.get_future();
+
+	std::thread task_td(std::move(task), 2, 10);
+	std::cout << "waiting for result" << std::endl;
+	std::cout << "task_thread:\t" << result.get() << '\n';
+	task_td.join();
+}
+
+int main()
+{
+	task_lambda();
+	task_bind();
+	task_thread();
+}
