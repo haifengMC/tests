ff10cd569e27022cae4bce18b6f2430f0ed2b1a4
haifengMC <1745502795@qq.com>
2020-07-02 18:36:00 +0800

    测试函数模版无法特例多类型模版

--- 05_模版/11_只有返回值的特例/main.cpp
+++ 05_模版/11_只有返回值的特例/main.cpp
@@ -16,11 +16,26 @@ char func<char>()
 	return 'h';
 }
 
+//template<typename T1, typename T2>
+//std::pair<T1, T2> func<pair<T1, T2>>()
+//{
+//	cout << "func 3" << endl;
+//	return std::make_pair(T1(), T2());
+//}
+
+template<typename T1, typename T2>
+ostream& operator<<(ostream& os, const pair<T1, T2 >& p)
+{
+	cout << "{" << p.first << "," << p.second << "}" << endl;
+	return os;
+}
+
 
 int main()
 {
 	cout << func<int>() << endl;
 	cout << func<char>() << endl;
+	cout << func<pair<int, int>>() << endl;
 
 	return 0;
 }
