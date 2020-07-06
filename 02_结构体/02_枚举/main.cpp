ec46fed82846a45b8ce880229ecc53d8be56d8b9
haifengMC <1745502795@qq.com>
2020-07-06 20:59:54 +0800

    强类型枚举测试

--- 02_结构体/02_枚举/main.cpp
+++ 02_结构体/02_枚举/main.cpp
@@ -2,18 +2,29 @@
 
 using namespace std;
 
-enum class E : size_t
+#define ENUM_BEG(E)\
+	enum class E : size_t
+
+#define ENUM_END(E) ;\
+	ostream& operator<<(ostream&os, const E& e)\
+	{\
+		return os << (size_t)e;\
+	}
+
+
+ENUM_BEG(E)
 {
 	a = 10,
 	b,
-};
+}
+ENUM_END(E)
 
 int main()
 {
 	E e = E::a;
 	int i = (int)e;
 	cout << i << endl;
-	//cout << e << endl;
+	cout << e << endl;
 
 
 	return 0;
