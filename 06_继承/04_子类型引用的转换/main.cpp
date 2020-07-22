06d5119b5646d798660dec8ab01045c34d70278f
haifengMC <1745502795@qq.com>
2020-07-22 15:51:00 +0800

    测试动态类型转换

---
+++ 06_继承/04_子类型引用的转换/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+public:
+	A() {}
+	virtual void pt() const { cout << "A" << endl; }
+};
+
+class B1 : public A
+{
+	int i;
+public:
+	B1(int i) : i(i) {}
+	virtual void pt() const { cout << "B1" << " " << i << endl; }
+};
+
+
+int main()
+{
+	A* a1 = new B1(2);
+	A* a2 = dynamic_cast<const B1*>(a1) ? new B1(dynamic_cast<const B1&>(*a1)) : NULL;
+	a1->pt();
+	a2->pt();
+
+	return 0;
+}
