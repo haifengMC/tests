87995fff640ec77f5f15ce18db4c571ca3277fb9
haifengMC <1745502795@qq.com>
2020-08-11 16:58:10 +0800

    再测除法运算符

--- 03_运算符/03_除法运算符/main.cpp
+++ 03_运算符/03_除法运算符/main.cpp
@@ -6,6 +6,10 @@ int main()
 {
 	cout << 100 / 200 * 200 << endl;
 	cout << 100 / 200.0 * 200 << endl;
+	
+	cout << (int)(1 * 1.5) << endl;
+	cout << (int)(2 * 1.5) << endl;
+	cout << (int)(3 * 1.5) << endl;
 
 
 	return 0;
