cf2aad9f37b6c4972e4b8e75a9748943c1068f9c
haifengMC <1745502795@qq.com>
2020-09-03 22:12:25 +0800

    子类覆盖父类方法不能多态

---
+++ 06_继承/05_子类覆盖父类方法/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+class A
+{
+public:
+	virtual void func1() { cout << "A1" << endl; }
+	void func2() { cout << "A2" << endl; }
+};
+
+class B : public A
+{
+public:
+	void func1() { cout << "B1" << endl; }
+	void func2() { cout << "B2" << endl; }
+};
+
+int main()
+{
+	A* pa = new B;
+	pa->func1();
+	pa->func2();
+
+	return 0;
+}
