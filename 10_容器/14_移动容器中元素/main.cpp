e3dd2537878fcc8d74a289a938fc5475c8494e97
haifengMC <1745502795@qq.com>
2020-09-08 22:00:49 +0800

    获取移动后元素的迭代器

--- 10_容器/14_移动容器中元素/main.cpp
+++ 10_容器/14_移动容器中元素/main.cpp
@@ -29,12 +29,12 @@ public:
 
 	void spliceBack(DataList& other, list<Data>::iterator it)
 	{
-		l.splice(l.end(), other.l, it);
+		l.splice(l.begin(), other.l, it);
 	}
 
 	void spliceBack(DataList& other)
 	{
-		l.splice(l.end(), other.l);
+		l.splice(l.begin(), other.l);
 	}
 };
 
@@ -48,14 +48,22 @@ int main()
 	ct(l2.l);
 	cout << endl;
 
+	auto itOld1 = l1.l.begin();
 	l1.spliceBack(l2, l2.l.begin());
 	ct(l1.l);
 	ct(l2.l);
 	cout << endl;
+	for (auto it = l1.l.begin(); it != itOld1; ++it)
+		cout << *it << " ";
+	cout << endl;
 
+	auto itOld2 = l2.l.begin();
 	l2.spliceBack(l1);
 	ct(l1.l);
 	ct(l2.l);
+	cout << endl;
+	for (auto it = l2.l.begin(); it != itOld2; ++it)
+		cout << *it << " ";
 
 
 	return 0;
