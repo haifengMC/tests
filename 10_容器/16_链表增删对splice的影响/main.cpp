49b9ac120961aa1db2c100b58853df65e2aa7fb0
haifengMC <1745502795@qq.com>
2020-09-09 21:49:04 +0800

    移动后迭代器仍有效并指向新容器的位置

---
+++ 10_容器/16_链表增删对splice的影响/main.cpp
@@ -0,0 +1,24 @@
+#include <list>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	list<int> l1 = { 1, 2, 3 };
+	list<int> l2 = { 4, 5, 6 };
+
+	auto it1 = l2.begin();
+	auto it2 = it1;
+	++it2;
+	cout << *it1 << " " << *it2 << endl;
+
+	l1.splice(l1.begin(), l2, it1);
+	cout << *it1 << endl;
+	cout << *++it1 << endl;
+	cout << (it2 == l2.begin()) << endl;
+	cout << *++it2 << endl;
+
+
+	return 0;
+}
