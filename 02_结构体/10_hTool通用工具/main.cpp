08fd6a8a925da3d095286f51365d9a4f9e026a32
haifengMC <1745502795@qq.com>
2020-09-17 20:43:46 +0800

    移入变量时需使用move移动

--- 02_结构体/10_hTool通用工具/main.cpp
+++ 02_结构体/10_hTool通用工具/main.cpp
@@ -8,7 +8,7 @@ using namespace hTool;
 TEST_INIT(Tst, testAll)
 
 struct A { A(const A&) = default; A(A&&) = delete; };
-struct B { B(const B&) = delete; B(B&&) = default; };
+struct B { B() = default; B(const B&) = delete; B(B&&) = default; };
 struct C { C(const C&) = default; C(C&&) = default; };
 struct D { D(const D&) = delete; D(D&&) = delete; };
 
@@ -30,6 +30,19 @@ TEST(Tst, 判断构造器是否可构造)
 		"def " << isConstructible<D>(Constructible::Default) << ", "
 		"cpy " << isConstructible<D>(Constructible::Copy) << ", "
 		"mov " << isConstructible<D>(Constructible::Move) << endl;
+}
+
+void insert(map<int, B>& m, B& b)
+{
+	cout << "insert B" << endl;
+	m.insert(make_pair(1, move(b)));
+}
+
+TEST(Tst, 将元素正确拷贝或移入容器)
+{
+	B b;
+	map<int, B> m;
+	insert(m, b);
 }
 
 int main()
