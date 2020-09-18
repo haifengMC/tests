cf81f65cfa9b517f341b285a7eb65f226e7aa7bd
haifengMC <1745502795@qq.com>
2020-09-18 16:01:59 +0800

    使id生成器支持移动元素

--- 10_容器/09_优化唯一id生成器/main.cpp
+++ 10_容器/09_优化唯一id生成器/main.cpp
@@ -8,21 +8,53 @@ using namespace std;
 
 TEST_INIT(Tst, testAll)
 
-class UniqueTst : public hTool::hUniqueMapVal<size_t, UniqueTst>
+class UniqueTst1 : public hTool::hUniqueMapVal<size_t, UniqueTst1>
 {
 	size_t id = 0;
 public:
-	UniqueTst() : hTool::hUniqueMapVal<size_t, UniqueTst>(id, this) {}
-	UniqueTst(size_t i) : hTool::hUniqueMapVal<size_t, UniqueTst>(id, this) { id = i; }
-	UniqueTst(const UniqueTst& t) : hTool::hUniqueMapVal<size_t, UniqueTst>(id, this) { id = t; }
+	UniqueTst1() : hTool::hUniqueMapVal<size_t, UniqueTst1>(id, this) {}
+	UniqueTst1(size_t i) : hTool::hUniqueMapVal<size_t, UniqueTst1>(id, this) { id = i; }
+	UniqueTst1(const UniqueTst1& t) : hTool::hUniqueMapVal<size_t, UniqueTst1>(id, this) { id = t; }
+};
+
+class UniqueTst2 : public hTool::hUniqueMapVal<size_t, UniqueTst2>
+{
+	size_t id = 0;
+public:
+	UniqueTst2() : hTool::hUniqueMapVal<size_t, UniqueTst2>(id, this) {}
+	UniqueTst2(size_t i) : hTool::hUniqueMapVal<size_t, UniqueTst2>(id, this) { id = i; }
+	UniqueTst2(const UniqueTst2&& t) : hTool::hUniqueMapVal<size_t, UniqueTst2>(id, this) { id = t.id; }
+};
+
+struct A { string s; A(const string& s) : s(s) {} };
+struct B { string s; B(const string& s) : s(s) {} };
+class UniqueTst3 : public hTool::hUniqueMapVal<size_t, UniqueTst3>
+{
+	size_t id = 0;
+	hTool::hAutoPtr<A> pA;
+	hTool::hAutoPtr<B> pB;
+public:
+	UniqueTst3() : hTool::hUniqueMapVal<size_t, UniqueTst3>(id, this)
+	{
+		pA.emplace("abc");
+		pB.emplace("def");
+	}
+	UniqueTst3(size_t i) : hTool::hUniqueMapVal<size_t, UniqueTst3>(id, this) { id = i; }
+	UniqueTst3(UniqueTst3&& t) noexcept : hTool::hUniqueMapVal<size_t, UniqueTst3>(id, this)
+	{
+		cout << "move" << endl;
+		id = t.id;
+		pA = move(t.pA);
+		pB = move(t.pB);
+	}
 };
 
 TEST(Tst, Tst1)
 {
-	map<size_t, UniqueTst> m;
-	hTool::hUniqueIdGen<size_t, UniqueTst> gen(m, 10);
+	map<size_t, UniqueTst1> m;
+	hTool::hUniqueIdGen<size_t, UniqueTst1> gen(m, 10);
 	gen.resize(100, 500);
-	UniqueTst t1;
+	UniqueTst1 t1;
 	gen.insert(t1);
 	
 	cout << gen << endl;
@@ -30,17 +62,17 @@ TEST(Tst, Tst1)
 
 TEST(Tst, Tst2)
 {
-	UniqueTst t1(10), t2(20);
+	UniqueTst1 t1(10), t2(20);
 	cout << t1 << ", " << t2 << endl;
-	UniqueTst t3 = t2;
+	UniqueTst1 t3 = t2;
 	cout << t2 << ", " << t3 << endl;
 	t2(200);
 	cout << t2 << ", " << t3 << endl;
 	t3(300);
 	cout << t1 << "," << t2 << ", " << t3 << endl;
 
-	map<size_t, UniqueTst> m;
-	hTool::hUniqueIdGen<size_t, UniqueTst> gen(m, 10);
+	map<size_t, UniqueTst1> m;
+	hTool::hUniqueIdGen<size_t, UniqueTst1> gen(m, 10);
 	gen.resize(100, 500);
 	gen << t1 << t2 << t3;
 	cout << gen << endl;
@@ -49,8 +81,8 @@ TEST(Tst, Tst2)
 
 TEST(Tst, Tst3)
 {
-	map<size_t, UniqueTst> m;
-	hTool::hUniqueIdGen<size_t, UniqueTst> gen(m, 10);
+	map<size_t, UniqueTst1> m;
+	hTool::hUniqueIdGen<size_t, UniqueTst1> gen(m, 10);
 	cout << gen << endl;
 	gen << 3;
 	cout << gen << " " << gen.back()->first << endl;
@@ -97,12 +129,93 @@ TEST(Tst, Tst3)
 	cout << gen << endl;
 	gen.resize(3, 7);
 	cout << gen << endl;
-	UniqueTst t1(10);
+	UniqueTst1 t1(10);
 	cout << t1 << endl;
 	gen << t1;
 	cout << gen << " " << t1 << endl;
 	gen.erase(t1);
 	cout << gen << " " << t1 << endl;
+}
+
+TEST(Tst, Tst4)
+{
+	map<size_t, UniqueTst2> m;
+	hTool::hUniqueIdGen<size_t, UniqueTst2> gen(m, 10);
+	gen.resize(100, 500);
+	UniqueTst2 t1;
+	gen.insert(move(t1));
+	gen << move(t1);
+
+	cout << gen << endl;
+}
+
+TEST(Tst, Tst5)
+{
+	map<size_t, UniqueTst2> m;
+	hTool::hUniqueIdGen<size_t, UniqueTst2> gen(m, 10);
+	cout << gen << endl;
+	gen << 3;
+	cout << gen << " " << gen.back()->first << endl;
+	gen << 1;
+	cout << gen << " " << gen.back()->first << endl;
+	gen << gen.getInvaild();
+	cout << gen << endl;
+	gen.genCheck();
+	cout << gen << endl;
+	gen.resize(1, 5);
+	cout << gen << endl;
+	gen << gen.getInvaild();
+	cout << gen << endl;
+	gen << gen.getInvaild();
+	cout << gen << endl;
+	gen << gen.getInvaild();
+	cout << gen << endl;
+	gen.resize(2, 4);
+	cout << gen << endl;
+	gen.resize(1, 10);
+	cout << gen << endl;
+	gen << gen.getInvaild();
+	cout << gen << endl;
+	gen << gen.getInvaild();
+	cout << gen << endl;
+	gen << gen.getInvaild();
+	cout << gen << endl;
+	gen << gen.getInvaild();
+	cout << gen << endl;
+	gen << gen.getInvaild();
+	cout << gen << endl;
+	gen << gen.getInvaild();
+	cout << gen << endl;
+	gen << gen.getInvaild();
+	cout << gen << endl;
+	gen << gen.getInvaild();
+	cout << gen << endl;
+	gen.resize(2, 8);
+	cout << gen << endl;
+	gen.putKey(2);
+	gen.putKey(3);
+	gen.putKey(5);
+	gen.putKey(7);
+	cout << gen << endl;
+	gen.resize(3, 7);
+	cout << gen << endl;
+	UniqueTst2 t1(10);
+	cout << t1 << endl;
+	gen << t1;
+	cout << gen << " " << t1 << endl;
+	gen.erase(t1);
+	cout << gen << " " << t1 << endl;
+}
+
+TEST(Tst, Tst6)
+{
+	map<size_t, UniqueTst3> m;
+	hTool::hUniqueIdGen<size_t, UniqueTst3> gen(m, 10);
+	gen.resize(100, 500);
+	UniqueTst3 t1;
+	gen.insert(t1);
+
+	cout << gen << endl;
 }
 
 int main()
