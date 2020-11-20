b0521b257959fe8c7cc1a537fc68d9490d5f8e7f
haifengMC <1745502795@qq.com>
2020-11-20 21:01:12 +0800

    resize不带默认参数的vector

--- 10_容器/23_vector随机访问特性/main.cpp
+++ 10_容器/23_vector随机访问特性/main.cpp
@@ -3,7 +3,18 @@
 
 using namespace std;
 
+struct A
+{
+	A(int i) : i(i) {}
+	int i;
+};
 
+void p(const vector<A>& vA)
+{
+	for (auto& a : vA)
+		cout << a.i << " ";
+	cout << endl;
+}
 
 int main()
 {
@@ -14,6 +25,14 @@ int main()
 	v.resize(20);
 	cout << v.capacity() << " " << v.size() << endl;
 	cout << v[2] << endl;
+
+	vector<A> vA = { 1,2,3 };
+	cout << vA.capacity() << " " << vA.size() << endl;
+	p(vA);
+	vA.resize(10, 20);
+	cout << vA.capacity() << " " << vA.size() << endl;
+	p(vA);
+
 
 	return 0;
 }
