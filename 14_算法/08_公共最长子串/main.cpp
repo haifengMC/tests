59756f4edb8977c111df2021e6f257335113a7c7
haifengMC <1745502795@qq.com>
2021-05-30 00:43:06 +0800

    公共最长子串

--- 14_算法/08_公共最长子串/main.cpp
+++ 14_算法/08_公共最长子串/main.cpp
@@ -1,5 +1,7 @@
 #include <string>
 #include <list>
+#include <vector>
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
@@ -7,8 +9,8 @@ using namespace std;
 //longest common sequence
 struct LCSPos
 {
-	string::size_type _pos1 = string::npos;
-	string::size_type _pos2 = string::npos;
+	size_t _pos1 = string::npos;
+	size_t _pos2 = string::npos;
 	string _subStr;
 };
 
@@ -21,24 +23,66 @@ struct LCSData
 	const string& _s2;
 
 	LCSData(string& s1, string& s2) : _s1(s1), _s2(s2) {}
-	void solution()
-	{
-
-
-		for (auto ch1 : _s1)
-		{
-			for (auto _ch2 : _s2)
-			{
-
-			}
-		}
-	}
+	void solution();
+	void print();
 };
-
 
 int main()
 {
-
+	string s1 = "abab", s2="ab";
+	
+	do
+	{
+		LCSData dt(s1, s2);
+		dt.solution();
+		dt.print();
+	} while (cin >> s1 >> s2);
 
 	return 0;
+}
+
+void LCSData::solution()
+{
+	if (_s1.empty() || _s2.empty())
+		return;
+
+	vector<size_t> preBuf(_s2.size());
+	vector<size_t> curBuf(_s2.size());
+
+	for (size_t i1 = 0; i1 < _s1.size(); ++i1)
+	{
+		for (size_t i2 = 0; i2 < _s2.size(); ++i2)
+		{
+			curBuf[i2] = i1 && i2 ? preBuf[i2 - 1] : 0;
+
+			if (_s1[i1] != _s2[i2])
+				continue;
+
+			++curBuf[i2];
+			if (curBuf[i2] < max<size_t>(2, _len))
+				continue;
+
+			if (_len < curBuf[i2])
+			{
+				_len = curBuf[i2];
+				_posList.clear();
+			}
+
+			_posList.emplace_back();
+			_posList.back()._pos1 = i1 + 1 - _len;
+			_posList.back()._pos2 = i2 + 1 - _len;
+			_posList.back()._subStr = _s1.substr(i1 + 1 - _len, _len);
+		}
+		swap(preBuf, curBuf);
+	}
+}
+
+void LCSData::print()
+{
+	cout << "s1:" << _s1 << endl;
+	cout << "s2:" << _s2 << endl;
+	cout << "len:" << _len << endl;
+
+	for (auto& pos : _posList)
+		cout << "[" << pos._pos1 << "," << pos._pos2 << "]" << pos._subStr << endl;
 }
