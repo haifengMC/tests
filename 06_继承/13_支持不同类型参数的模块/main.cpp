f7e7cd893b91ca07da1fcbb0a7a82b138991fd4c
haifengMC <1745502795@qq.com>
2021-07-06 19:27:37 +0800

    支持不用类型参数的模块

---
+++ 06_继承/13_支持不同类型参数的模块/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+struct A
+{
+	int i1 = 1;
+	int i2 = 2;
+};
+
+struct B
+{
+	int i1 = 3;
+	int i2 = 4;
+};
+
+struct FuncBase
+{
+	virtual void printCallback() = 0;
+};
+
+template <typename T>
+struct PrintFunc : public FuncBase
+{
+	void printCallback()
+	{
+		printIt(T());
+	}
+
+	virtual void printIt(const T& a)
+	{
+		cout << "PrintFunc" << endl;
+	}
+};
+
+struct FuncA : public PrintFunc<A>
+{
+	void printIt(const A& a)
+	{
+		cout << "A " << a.i1 << "," << a.i2 << endl;
+	}
+};
+
+struct FuncB : public PrintFunc<B>
+{
+	void printIt(const B& b)
+	{
+		cout << "B " << b.i1 << "," << b.i2 << endl;
+	}
+};
+
+int main()
+{
+	FuncBase* FuncArr[] = { new FuncA, new FuncB };
+	for (auto pFunc : FuncArr)
+	{
+		pFunc->printCallback();
+		delete pFunc;
+	}
+
+	return 0;
+}
