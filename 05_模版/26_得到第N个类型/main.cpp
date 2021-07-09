eb36ada52741e9d130e72490350a88b701131dc3
haifengMC <1745502795@qq.com>
2021-07-09 17:49:22 +0800

    得到第N个模版的类型

---
+++ 05_模版/26_得到第N个类型/main.cpp
@@ -0,0 +1,41 @@
+#include <map>
+#include <iostream>
+#include <ratio>
+#include <typeinfo>
+#include <type_traits>
+
+template <size_t N, class...> 
+struct GetNType { using Type = void; };
+template<typename _First, typename... _Rest>
+struct GetNType<1, _First, _Rest...> { using Type = _First; };
+template<size_t N, typename _FirstTy, typename... _Rest>
+struct GetNType <N, _FirstTy, _Rest...> : public GetNType<std::ratio_subtract<std::ratio<N>, std::ratio<1>>::num, _Rest...> {};
+
+class A {};
+
+#define Alias
+#ifdef Alias
+template <size_t N, typename... _Ty>
+using SubType = typename GetNType<N, _Ty...>::Type;
+#endif
+
+int main()
+{
+	using namespace std;
+	cout << typeid(GetNType<0, int, char, float, A>::Type).name() << endl;
+	cout << typeid(GetNType<1, int, char, float, A>::Type).name() << endl;
+	cout << typeid(GetNType<2, int, char, float, A>::Type).name() << endl;
+	cout << typeid(GetNType<3, int, char, float, A>::Type).name() << endl;
+	cout << typeid(GetNType<4, int, char, float, A>::Type).name() << endl;
+	cout << typeid(GetNType<5, int, char, float, A>::Type).name() << endl;
+	cout << endl;
+#ifdef Alias
+	cout << typeid(SubType<0, int, char, float, A>).name() << endl;
+	cout << typeid(SubType<1, int, char, float, A>).name() << endl;
+	cout << typeid(SubType<2, int, char, float, A>).name() << endl;
+	cout << typeid(SubType<3, int, char, float, A>).name() << endl;
+	cout << typeid(SubType<4, int, char, float, A>).name() << endl;
+	cout << typeid(SubType<5, int, char, float, A>).name() << endl;
+#endif
+	return 0;
+}
