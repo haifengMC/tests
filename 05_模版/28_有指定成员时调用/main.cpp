52e5a25c0d2987cfa2e3227fa0aab60132a1f082
haifengMC <1745502795@qq.com>
2021-07-13 13:43:50 +0800

    利用is_base_of实现，是某类型子类时执行函数，不是时忽略而不报错

--- 05_模版/28_有指定成员时调用/main.cpp
+++ 05_模版/28_有指定成员时调用/main.cpp
@@ -1,30 +1,50 @@
 #include <iostream>
-#include <functional>
 
 using namespace std;
 
-
-template <typename Ty>
-struct CallFunc
+namespace hCallFuncDetail
 {
-	template <typename Ty, typename Func>
-	void operator() (Ty& t, Func) { cout << "Ty::void" << endl; }
-	template <typename Ty>
-	void operator() (Ty& t, void(Ty::* f)() = &Ty::f) { cout << "Ty::f" << endl; }
+	template <typename Base, bool isBase>
+	struct _hCallFunc
+	{
+		template <typename Func, typename Ty>
+		static void exec(Func, Ty&) {}
+	};
+
+	template <typename Base>
+	struct _hCallFunc<Base, true>
+	{
+		template <typename Func, typename Ty>
+		static void exec(Func f, Ty& t) { return f(t); }
+	};
+}
+
+template <typename Base>
+struct hCallFunc
+{
+	template <typename Func, typename Ty>
+	static void exec(Func f, Ty& t)
+	{ 
+		hCallFuncDetail::_hCallFunc<Base, std::is_base_of_v<Base, Ty>>::exec(f, t);
+	}
 };
 
-
-struct A { void f() { cout << "A::f()" << endl; } };
+struct Base { virtual void f() { cout << "Base::f()" << endl; } };
+struct A : public Base { void f() { cout << "A::f()" << endl; } };
 struct B { void f() { cout << "B::f()" << endl; } };
-struct C { void k() { cout << "C::k()" << endl; } };
+struct C : public Base { void k() { cout << "C::k()" << endl; } };
+void f(Base& b) { b.f(); }
+template <typename Ty>
+void callBaseF(Ty t) { hCallFunc<Base>::exec(f, t); }
 
 int main()
 {
-	A a;
-	CallFunc<A>()(a, &A::f);
-	B b;
-	CallFunc<B>()(b, &A::f);
-	C c;
-	CallFunc<C>()(c, &A::f);
+	cout << "exec(a)" << endl;
+	callBaseF(A());
+	cout << "exec(b)" << endl;
+	callBaseF(B());
+	cout << "exec(c)" << endl;
+	callBaseF(C());
+
 	return 0;
 }
